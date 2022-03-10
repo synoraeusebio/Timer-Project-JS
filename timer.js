60c7b76445aaa2681d8636519255c58cd4f58b9a
Synora Eusebio <synora.eusebio@a8c.com>
2022-03-09 20:06:06 -0500

    js and html files

---
+++ timer.js
@@ -0,0 +1,28 @@
+//Timer
+
+
+/*
+timer update: 
+adding the counter back so we see just the "counter" counting down instead of a long time string
+*/
+
+function startCountdown(seconds) {
+  let startTime = Date.now() //
+  let counter = seconds;
+
+  const interval = setInterval(() => {
+    console.log(counter); 
+    counter -= 1; 
+    
+
+    if(Date.now() >= startTime + (seconds * 1000)) {
+      clearInterval(interval);
+      console.log("The countdown has ended!");
+    }
+  }, 1000);
+}
+
+
+startCountdown(5);
+
+
