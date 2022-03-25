94a3eb300f49fd8545ef8183ba97e754069dad17
Matt Gawarecki <matt.gawarecki@gmail.com>
2022-03-25 12:53:26 -0500

    Add convenience functions for timer elements

--- timer.js
+++ timer.js
@@ -6,23 +6,31 @@ timer update:
 adding the counter back so we see just the "counter" counting down instead of a long time string
 */
 
+function getCountdown() {
+  return document.querySelector( '.timer__countdown' );
+}
+
+function getTimeDisplay() {
+  return document.querySelector( '.timer__time-display' );
+}
+
+function getEndedDisplay() {
+  return document.querySelector( '.timer__ended' );
+}
+
 function startCountdown(seconds) {
   let startTime = Date.now() //
   let counter = seconds;
 
   const interval = setInterval(() => {
-    const timeDisplayElement = document.querySelector( '.timer__time-display' );
     counter -= 1; 
-    timeDisplayElement.innerHTML = counter; 
+    getTimeDisplay().innerHTML = counter; 
 
     if(Date.now() >= startTime + (seconds * 1000)) {
       clearInterval(interval);
 
-      const countdownElement = document.querySelector( '.timer__countdown' );
-      countdownElement.style.display = 'none';
-
-      const endedElement = document.querySelector( '.timer__ended' );
-      endedElement.style.display = 'initial';
+      getCountdown().style.display = 'none';
+      getEndedDisplay().style.display = 'initial';
     }
   }, 1000);
 }
